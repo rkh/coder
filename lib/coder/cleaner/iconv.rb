975448fc40d9f1c6429f56ec40b62a0f485d9217
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-09-21 12:37:22 +0200

    fix iconv loading

--- lib/coder/cleaner/iconv.rb
+++ lib/coder/cleaner/iconv.rb
@@ -4,10 +4,23 @@ require 'stringio'
 module Coder
   module Cleaner
     class Iconv
+      def self.load_iconv
+        return if defined? ::Iconv
+        stderr_was, $stderr = $stderr, StringIO.new
+        require 'iconv'
+      ensure
+        $stderr = stderr_was if stderr_was
+      end
+
+      def self.new(*)
+        load_iconv
+        super
+      end
+
       def self.available?
-        quiet { require 'iconv' } unless defined? ::Iconv
+        load_iconv
         !!::Iconv.conv("iso-8859-1//ignore", "utf-8", "\305\253" + "a"*8160)
-      rescue Exception
+      rescue Exception => e
         false
       end
 
@@ -21,15 +34,6 @@ module Coder
         @iconv.iconv(str).gsub("\0", "")
       rescue ::Iconv::Failure => e
         raise Coder::Error, e.message
-      end
-
-      private
-
-      def quiet
-        stderr_was, $stderr = $stderr, StringIO.new
-        yield
-      ensure
-        $stderr = stderr_was
       end
     end
   end
